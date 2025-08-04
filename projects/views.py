830416da57157ea2c505b339aeeef020bfcd87f9
dankoche <chevesichdanko@gmail.com>
2025-08-04 00:02:47 -0400

    "Fix"

--- projects/views.py
+++ projects/views.py
@@ -3,4 +3,9 @@ from .models import Project
 
 def home(request):
     projects = Project.objects.all()
+    for project in projects:
+        if project.technologies:
+            project.technologies_list = [tech.strip() for tech in project.technologies.split(',')]
+        else:
+            project.technologies_list = []
     return render(request, 'index.html', {'projects': projects})
