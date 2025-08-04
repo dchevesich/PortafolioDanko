238f786b431c0b87bacb5d5f7fd33fb5ba80e893
dankoche <chevesichdanko@gmail.com>
2025-08-04 00:19:38 -0400

    "Update"

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -73,8 +73,8 @@ DATABASES = {
 }
 
 # Heroku: Update database configuration from DATABASE_URL.
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+if 'DATABASE_URL' in os.environ:
+    DATABASES['default'] = dj_database_url.config(conn_max_age=500)
 
 
 # Password validation
